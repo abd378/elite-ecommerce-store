daeb8fa4f7cb5c28c8aeee4d21712ad3e4e3db3f
abdosman81234 <oabed5527@gmail.com>
2026-05-17 01:52:57 +0300

    fix duplicated supabase client

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,14 +1,6 @@
- HEAD
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
- c7e17146a80975254839b4a30d833ff59ff121ff
 export const supabase = createClient(supabaseUrl, supabaseKey);
