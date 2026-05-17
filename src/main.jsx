f8eccbed8c544300a1a65111e01732d824f68717
abdosman81234 <oabed5527@gmail.com>
2026-05-17 16:57:04 +0300

    fix onesignal prompt

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
@@ -9,19 +8,31 @@ import OneSignal from "react-onesignal";
 async function initOneSignal() {
   await OneSignal.init({
     appId: "d0c25769-48c3-4bfc-a279-c9a3cad3a442",
-    notifyButton: {
-      enable: true,
-    },
     allowLocalhostAsSecureOrigin: true,
+    promptOptions: {
+      slidedown: {
+        prompts: [
+          {
+            type: "push",
+            autoPrompt: true,
+            text: {
+              actionMessage: "We want to notify you when a new order arrives.",
+              acceptButton: "Allow",
+              cancelButton: "Later",
+            },
+          },
+        ],
+      },
+    },
   });
+
+  OneSignal.showSlidedownPrompt();
 }
 
 initOneSignal();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
