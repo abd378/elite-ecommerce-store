d1cb90f21eeb4569798a1a97a284e0e8f2549b04
abdosman81234 <oabed5527@gmail.com>
2026-05-17 20:01:37 +0300

    force button visible

--- src/App.jsx
+++ src/App.jsx
@@ -241,26 +241,23 @@ function App() {
                 <div className="profile-icon">
                   {user.name.charAt(0).toUpperCase()}
                 </div>
+<span>{user.name}</span>
 
-                <span>{user.name}</span>
-
-                {isAdmin && (
-                  <button
-                    onClick={enableSound}
-                    style={{
-                      marginLeft: "10px",
-                      padding: "10px 14px",
-                      border: "none",
-                      borderRadius: "10px",
-                      background: "orange",
-                      color: "white",
-                      fontWeight: "bold",
-                      cursor: "pointer",
-                    }}
-                  >
-                    🔔 Enable Sound
-                  </button>
-                )}
+<button
+  onClick={enableSound}
+  style={{
+    marginLeft: "10px",
+    padding: "10px 14px",
+    border: "none",
+    borderRadius: "10px",
+    background: "orange",
+    color: "white",
+    fontWeight: "bold",
+    cursor: "pointer",
+  }}
+>
+  🔔 Enable Sound
+</button>
 
                 <button
                   onClick={logout}
