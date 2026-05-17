110ab7041ca1e5bd2e433e1924f34c5f53978f91
abdosman81234 <oabed5527@gmail.com>
2026-05-17 22:11:00 +0300

    remove broken onesignal init

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-import OneSignal from "react-onesignal";
-
-async function initOneSignal() {
-  await OneSignal.init({
-    appId: "d0c25769-48c3-4bfc-a279-c9a3cad3a442",
-    allowLocalhostAsSecureOrigin: true,
-  });
-}
-
-initOneSignal();
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
