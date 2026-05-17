c19c518a1d4e3247a9c4d74b9b63f5c1c77fe9a1
abdosman81234 <oabed5527@gmail.com>
2026-05-17 22:08:27 +0300

    fix onesignal init

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,14 @@ import "./index.css";
 
 import OneSignal from "react-onesignal";
 
-OneSignal.init({
-  appId: "d0c25769-48c3-4bfc-a279-c9a3cad3a442",
-  allowLocalhostAsSecureOrigin: true,
-});
+async function initOneSignal() {
+  await OneSignal.init({
+    appId: "d0c25769-48c3-4bfc-a279-c9a3cad3a442",
+    allowLocalhostAsSecureOrigin: true,
+  });
+}
+
+initOneSignal();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
