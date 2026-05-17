92b6a799ff378606a7b7f374f99258e223fe6d9b
abdosman81234 <oabed5527@gmail.com>
2026-05-17 16:49:15 +0300

    add onesignal notifications

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./index.css";
+
+import OneSignal from "react-onesignal";
+
+async function initOneSignal() {
+  await OneSignal.init({
+    appId: "d0c25769-48c3-4bfc-a279-c9a3cad3a442",
+    notifyButton: {
+      enable: true,
+    },
+    allowLocalhostAsSecureOrigin: true,
+  });
+}
+
+initOneSignal();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
