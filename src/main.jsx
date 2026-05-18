0a566ead700726b25a3c05dc8cdd817e0e47133a
abdosman81234 <oabed5527@gmail.com>
2026-05-18 10:18:40 +0300

    setup onesignal background push

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+window.OneSignalDeferred = window.OneSignalDeferred || [];
+
+window.OneSignalDeferred.push(async function (OneSignal) {
+  await OneSignal.init({
+    appId: "d0c25769-48c3-4bfc-a279-c9a3cad3a442",
+    serviceWorkerPath: "OneSignalSDKWorker.js",
+    serviceWorkerUpdaterPath: "OneSignalSDKUpdaterWorker.js",
+    notifyButton: {
+      enable: true,
+    },
+  });
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
